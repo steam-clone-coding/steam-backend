4425daa1abcd96fb4f76652cbe2baaf0fa4848b3
minturtle <minseok0425@kakao.com>
2023-07-16 14:58:05 +0900

    test for jenkins2

--- src/main/java/com/clonecoding/steam/SteamApplication.java
+++ src/main/java/com/clonecoding/steam/SteamApplication.java
@@ -3,7 +3,7 @@ package com.clonecoding.steam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//test
+//test jenkins
 @SpringBootApplication
 public class SteamApplication {
 
