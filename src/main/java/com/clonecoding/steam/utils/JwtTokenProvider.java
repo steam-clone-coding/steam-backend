5a8829099ac74f9c4ab230331ba8412c0d99382d
minturtle <minseok0425@kakao.com>
2023-07-27 15:02:29 +0900

    TEST : JwtTokenProvider 추가

--- src/main/java/com/clonecoding/steam/utils/JwtTokenProvider.java
+++ src/main/java/com/clonecoding/steam/utils/JwtTokenProvider.java
@@ -2,43 +2,46 @@ package com.clonecoding.steam.utils;
 
 
 import com.clonecoding.steam.entity.User;
+import com.clonecoding.steam.enums.UserAuthority;
 import com.clonecoding.steam.exceptions.ExceptionMessages;
 import io.jsonwebtoken.*;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Component
 public class JwtTokenProvider {
 
-    @Value("${jwt.secret}")
     private String secretKey;
 
-    @Value("${jwt.access-token-expire-time}")
-    private Long ACCESS_TOKEN_EXPIRE_TIME;
 
-    @Value("${jwt.refresh-token-expire-time}")
-    private Long REFRESH_TOKEN_EXPIRE_TIME;
+    private final Long ACCESS_TOKEN_EXPIRE_TIME;
 
+    private final Long REFRESH_TOKEN_EXPIRE_TIME;
+
+    public JwtTokenProvider(Environment env) {
+        secretKey = env.getProperty("jwt.secret");
+        ACCESS_TOKEN_EXPIRE_TIME = Long.parseLong(env.getProperty("jwt.access-token-expire-time"));
+        REFRESH_TOKEN_EXPIRE_TIME = Long.parseLong(env.getProperty("jwt.refresh-token-expire-time"));
+    }
 
     /**
      * methodName : sign
      * Author : Minseok Kim
      * description : 엑세스 토큰을 생성하는 메서드
      *
-     * @param : NtUser user - 토큰을 생성하려는 유저 정보
+     * @param : User user - 토큰을 생성하려는 유저 정보
      * @param : now : token을 생성하는 시간
      * @return : 엑세스 토큰 리턴
      */
     public String sign(User user, Date now) {
         Claims claims = Jwts.claims().setSubject(user.getUsername());
         claims.put("uid", user.getUid());
+        claims.put("userRole", user.getUserRole());
+
 
         Date expiryDate = new Date(now.getTime() + ACCESS_TOKEN_EXPIRE_TIME);
 
@@ -64,9 +67,9 @@ public class JwtTokenProvider {
 
             return TokenVerificationResult.builder()
                     .ok(true)
-                    .uid((Integer) claims.getBody().get("uid"))
+                    .uid(claims.getBody().get("uid").toString())
                     .userID(claims.getBody().getSubject())
-                    .category((String) claims.getBody().get("Category"))
+                    .userRole(UserAuthority.valueOf((String)claims.getBody().get("userRole")))
                     .build();
 
             // 토큰이 만료된 경우와, 순수 JWT 토큰 오류를 구별해 Exception을 Throw한다.
@@ -107,9 +110,9 @@ public class JwtTokenProvider {
     @Builder
     public static class TokenVerificationResult {
         private boolean ok;
-        private Integer uid;
+        private String uid;
         private String userID;
-        private String category;
+        private UserAuthority userRole;
         private String message;
     }
 }
---
+++ src/test/java/com/clonecoding/steam/utils/JwtTokenProviderTest.java
@@ -0,0 +1,127 @@
+package com.clonecoding.steam.utils;
+
+import com.clonecoding.steam.entity.User;
+import com.clonecoding.steam.enums.UserAuthority;
+import com.clonecoding.steam.exceptions.ExceptionMessages;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ActiveProfiles("test")
+@ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
+@ExtendWith(SpringExtension.class)
+@Import(JwtTokenProviderTest.TestConfig.class)
+public class JwtTokenProviderTest {
+
+
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+
+    private Long testRefreshTokenExpireTime = 1800000L;
+    private Long testAccessTokenExpireTime = 600000L;
+
+    @Test
+    @DisplayName("JwtTokenProvider를 주입 받고 테스트할 수 있다.")
+    void t1() throws Exception {
+        //then
+
+        Long accessTokenExpireTime = (Long) ReflectionTestUtils
+                .getField(jwtTokenProvider, "ACCESS_TOKEN_EXPIRE_TIME");
+
+        Long refreshTokenExpireTime = (Long) ReflectionTestUtils
+                .getField(jwtTokenProvider, "REFRESH_TOKEN_EXPIRE_TIME");
+
+        assertThat(jwtTokenProvider).isNotNull();
+
+        assertThat(accessTokenExpireTime).isEqualTo(testAccessTokenExpireTime);
+        assertThat(refreshTokenExpireTime).isEqualTo(testRefreshTokenExpireTime);
+    }
+
+    @Test
+    @DisplayName("User 정보를 담는  Access Token을 생성하고 다시 해독해 이를 조회할 수 있다.")
+    void t2() throws Exception {
+        //given
+        User testUser = User.builder()
+                .uid("test")
+                .userRole(UserAuthority.ROLE_USER)
+                .username("testUserId")
+                .build();
+        //when
+
+        Date now = new Date();
+        String accessToken = jwtTokenProvider.sign(testUser, now);
+
+        JwtTokenProvider.TokenVerificationResult result = jwtTokenProvider.verify(accessToken);
+
+        //then
+        assertThat(result).extracting("ok", "uid", "userID", "userRole")
+                .containsExactly(true, "test", "testUserId", UserAuthority.ROLE_USER);
+
+    }
+
+    @Test
+    @DisplayName("AccessToken이 만료되면 ExpiredJwtException이 throw된다.")
+    void t3() throws Exception {
+        //given
+        User testUser = User.builder()
+                .id(1L)
+                .userRole(UserAuthority.ROLE_USER)
+                .username("Hello")
+                .build();
+        //when
+        Date prevTokenDate = new Date(0L); //아주 옛날에 생성된 엑세스 토큰..
+        String accessToken = jwtTokenProvider.sign(testUser, prevTokenDate);
+
+        ThrowableAssert.ThrowingCallable throwableMethod = ()->jwtTokenProvider.verify(accessToken);
+
+        //then
+        assertThatThrownBy(throwableMethod).isInstanceOf(ExpiredJwtException.class);
+
+    }
+
+    @Test
+    @DisplayName("잘못된 토큰 값을 Verify하면 JwtException을 Throw한다.")
+    void t4() throws Exception {
+        //given
+        String invalidToken = "te1_asd24AD142awWQf_31342QWW2";
+        //when
+        ThrowableAssert.ThrowingCallable throwableMethod = ()->jwtTokenProvider.verify(invalidToken);
+        //then
+        assertThatThrownBy(throwableMethod).isInstanceOf(JwtException.class)
+                .hasMessage(ExceptionMessages.INVALID_TOKEN.getMessage());
+    }
+
+
+
+    @TestConfiguration
+    public static class TestConfig{
+
+        @Autowired
+        private Environment environment;
+
+        @Bean
+        public JwtTokenProvider jwtTokenProvider(){
+            return new JwtTokenProvider(environment);
+        }
+    }
+}
+
