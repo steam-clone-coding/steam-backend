13dfa3370082aa5a087ac5db02d022cc61b918a0
minturtle <minseok0425@kakao.com>
2023-08-22 22:08:51 +0900

    Feat: JSON을 사용해서 Multi Upload의 응답을 DTO객체로 변환할 수 있다.

--- src/test/java/com/clonecoding/steam/service/ImageServerServiceTest.java
+++ src/test/java/com/clonecoding/steam/service/ImageServerServiceTest.java
@@ -14,7 +14,7 @@ class ImageServerServiceTest {
 
 
     @Test
-    @DisplayName("JSON을 사용해서 DTO로 객체를 변환할 수 있다.")
+    @DisplayName("JSON을 사용해서 Single Upload의 응답을 DTO객체로 변환할 수 있다.")
     public void t1() throws Exception{
         //given
         ObjectMapper objectMapper = new ObjectMapper();
@@ -33,4 +33,43 @@ class ImageServerServiceTest {
         assertThat(actual.getCode()).isEqualTo(200);
         assertThat(actual.getMessage()).isEqualTo("File(s) successfully uploaded.");
     }
+
+    @Test
+    @DisplayName("JSON을 사용해서 Multi Upload의 응답을 DTO객체로 변환할 수 있다.")
+    public void t2() throws Exception{
+        //given
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String testJsonString = "{\n" +
+                "    \"code\": 200,\n" +
+                "    \"message\": \"File(s) successfully uploaded.\",\n" +
+                "    \"data\": [\n" +
+                "        {\n" +
+                "            \"file_name\": \"jpeg_jk3.jpg\",\n" +
+                "            \"file_id\": \"b3b2bc5b075f434692f71657afbae2c9\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"file_name\": \"png_jk.png\",\n" +
+                "            \"file_id\": \"20995dfcf94a49e7b6d34ccce744609c\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"file_name\": \"png_bts.png\",\n" +
+                "            \"file_id\": \"9d15ce7799dd499181bbc8cace4761b7\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"file_name\": \"jpg_cat.jpg\",\n" +
+                "            \"file_id\": \"5fd0f71238ed4086b9bb58859ac3b271\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+        //when
+
+        MultipleImageUploadResult actual = objectMapper.readValue(testJsonString, MultipleImageUploadResult.class);
+        //then
+        assertThat(actual.getCode()).isEqualTo(200);
+        assertThat(actual.getMessage()).isEqualTo("File(s) successfully uploaded.");
+    }
+
+
+
 }
