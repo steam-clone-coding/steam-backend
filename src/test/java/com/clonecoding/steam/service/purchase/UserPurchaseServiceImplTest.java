d9084885606780c44afa1227a890814082112ece
minturtle <minseok0425@kakao.com>
2023-10-10 21:46:09 +0900

    Test : 이미 User가 Cart에 추가된 게임을 다시 Cart에 추가하려고 한다면 Exception을 throw한다.

--- src/test/java/com/clonecoding/steam/service/purchase/UserPurchaseServiceImplTest.java
+++ src/test/java/com/clonecoding/steam/service/purchase/UserPurchaseServiceImplTest.java
@@ -7,7 +7,7 @@ import com.clonecoding.steam.entity.user.User;
 import com.clonecoding.steam.repository.game.GameRepository;
 import com.clonecoding.steam.repository.purchase.CartRepository;
 import com.clonecoding.steam.repository.user.UserRepository;
-import com.clonecoding.steam.utils.common.NanoIdProvider;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -57,6 +56,12 @@ class UserPurchaseServiceImplTest {
 
     }
 
+
+    @AfterEach
+    void tearDown() {
+        cartRepository.deleteAllInBatch();
+    }
+
     @Test
     @DisplayName("User의 Cart 목록에 게임을 추가할 수 있다.")
     public void addCart() throws Exception{
@@ -72,10 +77,23 @@ class UserPurchaseServiceImplTest {
     }
 
     @Test
+    @DisplayName("이미 User가 Cart에 추가된 게임을 다시 Cart에 추가하려고 한다면 Exception을 throw한다.")
+    public void testDuplicatedGameInCart() throws Exception{
+        //given
+        userPurchaseService.addCart(testUser.getUid(), testGame.getUid());
+        //when
+    
+        //then
+        assertThatThrownBy(()->userPurchaseService.addCart(testUser.getUid(), testGame.getUid()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
     @DisplayName("User의 Cart목록을 조회할 수 있다.")
     public void getCartListByUser() throws Exception{
         //given
-
+        userPurchaseService.addCart(testUser.getUid(), testGame.getUid());
+        
         //when
         PaginationListDto<CartDTO.Preview> result =
                 userPurchaseService.getCartList(testUser.getUid(), PageRequest.of(0, 10));
