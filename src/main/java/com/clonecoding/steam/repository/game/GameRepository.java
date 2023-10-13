ece4dc7e6fc215867418a5aca412bc253e8e0cf0
hyunjin <guswlsdl0121@gmail.com>
2023-10-13 16:34:44 +0900

    상점 관련 리포지토리 개발 및 테스트 코드 작성

--- src/main/java/com/clonecoding/steam/repository/game/GameRepository.java
+++ src/main/java/com/clonecoding/steam/repository/game/GameRepository.java
@@ -1,13 +1,35 @@
 package com.clonecoding.steam.repository.game;
 
 import com.clonecoding.steam.entity.game.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GameRepository extends CrudRepository<Game, Long> {
 
     Optional<Game> findByUid(String uid);
+
+
+    /**
+     * 게임 검색
+     * @param q 검색어 쿼리
+     * @param pageable 객체
+     * @return 페이지 반환
+     */
+    Page<Game> findByNameContaining(String q, Pageable pageable);
+
+
+    /**
+     * 해당 게임과 관련있는 게임을 5개 찾는 API, 지금은 랜덤 게임임.
+     * @param gameId
+     * @return
+     */
+    @Query(nativeQuery = true, value = "SELECT * FROM games WHERE game_id != ?1 ORDER BY RANDOM() LIMIT 5")
+    List<Game> findRelatedGamesRandomly(Long uid);
 }
